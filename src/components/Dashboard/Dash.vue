4b94a127132f4329291bd38c1bef831751f10319
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2025-03-18 12:05:33 -0400

    feat layout dash

--- src/components/Dashboard/Dash.vue
+++ src/components/Dashboard/Dash.vue
@@ -4,7 +4,7 @@
     <aside class="w-64 bg-white p-4 flex flex-col items-center">
       <div class="flex flex-col items-center mt-5 mx-auto">
         <img :src="imgGroup" alt="Logo" class="w-20 h-14 mb-2" />
-        <h1 class="mt-5 font-bold text-font-text text-[14px]">PAINEL DE DADOS</h1>
+        <h1 class="mt-5 font-semibold text-font-text text-[14px]">PAINEL DE DADOS</h1>
       </div>
       <nav class="mt-3">
         <button
@@ -28,10 +28,10 @@
     </aside>
 
     <!-- Main Content -->
-    <div class="flex-1 mt-11 ml-10">
+    <div class="flex-1 mt-11 ml-10 space-y-4">
       <h2 class="text-[32px] font-semibold mb-4">Estatísticas</h2>
-      <div class="flex gap-8 rounded-md">
-        <div class="w-[264px] h-[141px] bg-white p-5">
+      <div class="flex gap-6 rounded-md">
+        <div class="w-[264px] h-[141px] bg-white p-5 rounded-md">
           <div class="flex justify-between">
             <p class="text-font-dash text-xl">Downloads</p>
             <img :src="imgCloud" alt="Logo" class="w-[26px] h-[26px]" />
@@ -48,7 +48,7 @@
             </div>
           </div>
         </div>
-        <div class="w-[318px] h-[141px] bg-white p-5">
+        <div class="w-[318px] h-[141px] bg-white p-5 rounded-md">
           <div class="flex justify-between">
             <p class="text-font-dash text-xl">Avaliações</p>
             <img :src="imgStar" alt="Logo" class="w-[26px] h-[26px]" />
@@ -65,29 +65,77 @@
             </div>
           </div>
         </div>
-        <div class="w-[304px] h-[141px] bg-white p-5">
+        <div class="w-[304px] h-[141px] bg-white p-5 rounded-md">
           <div class="flex justify-between">
             <p class="text-font-dash text-xl">Erros</p>
             <img :src="imgTimes" alt="Logo" class="w-6 h-6" />
           </div>
-            <p class="text-[40px] font-bold">4</p>
-            <div class="flex">
-              <div class="flex gap-1 items-center">
-                <img :src="imgAndroid" alt="Logo" class="w-4 h-4" />
-                <p>2</p>
-              </div>
-              <div class="flex gap-1 items-center ml-6">
-                <img :src="imgApple" alt="Logo" class="w-4 h-4" />
-                <p>1</p>
-              </div>
+          <p class="text-[40px] font-bold">4</p>
+          <div class="flex">
+            <div class="flex gap-1 items-center">
+              <img :src="imgAndroid" alt="Logo" class="w-4 h-4" />
+              <p>2</p>
+            </div>
+            <div class="flex gap-1 items-center ml-6">
+              <img :src="imgApple" alt="Logo" class="w-4 h-4" />
+              <p>1</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--! feedbacks -->
+      <div class="p-6 bg-white rounded-lg w-[933px] shadow">
+        <h2 class="text-xl font-semibold mb-2">Feedbacks</h2>
+        <table class="w-full">
+          <thead>
+            <tr class="text-left text-font-table text-sm">
+              <th>Avaliação</th>
+              <th class="px-5">Data</th>
+              <th class="">Avaliação</th>
+              <th class="">Melhorias</th>
+              <th class="">Plataforma</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(feedback, index) in feedbacks" :key="index" class="mb-5">
+              <td class="text-font-text-dash text-[10px] w-[408px] h-[41px] text-justify">
+                {{ feedback.text }}
+              </td>
+              <td class="px-5 text-font-text-dash text-sm">{{ feedback.date }}</td>
+              <td class="text-font-text-dash text-sm">{{ feedback.rating }}</td>
+              <td class="text-font-text-dash text-sm whitespace-pre-line">{{ feedback.improvements }}
+              </td>
+              <td class="text-font-text-dash text-sm">{{ feedback.platform }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!--! final -->
+      <div class="px-6 py-4 bg-white rounded-lg w-[933px] h-[153px]">
+        <h2 class="text-xl font-semibold mb-4">Novas Funcionalidades</h2>
+        <div class="mr-8">
+          <div class="flex justify-between text-font-dash text-xs -mt-1">
+            <h3>Funcionalidade</h3>
+            <h3>Taxa De Uso</h3>
+          </div>
+          <div class="space-y-2 mt-2">
+            <div class="flex justify-between px-4 w-[876px] h-[27px]">
+              <h3>Veículo em Rota</h3>
+              <h3 class="text-font-taxa mr-3">92%</h3>
+            </div>
+            <div class="flex justify-between px-4 bg-font-line w-[876px] h-[27px]">
+              <h3>Avaliação de coleta</h3>
+              <h3 class="text-font-taxa mr-3">78%</h3>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import imgGroup from '@/assets/Group.png';
 import imgCloud from '@/assets/cloud.png';
 import imgStar from '@/assets/star.png';
@@ -97,4 +145,21 @@ import imgApple from '@/assets/apple.png';
 import { RiDashboardHorizontalLine } from "vue-icons-plus/ri";
 import { GoGear } from "vue-icons-plus/go";
 import { HiOutlineLogout } from "vue-icons-plus/hi";
+
+const feedbacks = ref([
+  {
+    text: "Aplicativo muito bom, porém poderiam atualizar mensalmente com os dados da qualidade do leite. Quando vira o mês o aplicativo apaga os dados da produção dos meses anteriores, não sendo possível consultar os meses anteriores. Antes não tinha esse problema.",
+    date: "01/04/24",
+    rating: "4",
+    improvements: "Dados de Produção   \n  Suporte ao Usuário",
+    platform: "Android"
+  },
+  {
+    text: "Aplicativo muito bom, porém poderiam atualizar mensalmente com os dados da qualidade do leite.",
+    date: "30/03/24",
+    rating: "5",
+    improvements: "-",
+    platform: "IOS"
+  }
+]);
 </script>
